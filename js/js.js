2ac03042234a965766a0731fecaf7e226d9259d2
yannclergue <clergue14@gmail.com>
2023-10-17 09:56:06 +0200

    Créer un reset
    Pour allez plus loin, un boutton proposant de créer une grille
    de la taille que l'on souhaite a été ajouté.
    
    En cliquant sur le bouton, une fonction anonyme se lance,
    commencant par demander le nombre de div que l'on souhaite, puis
    supprimer les anciennes, ensuite, ajouter les nouvelles et enfin,
    remettre la fonction "couleur" pour que l'éffet de hover marche toujours

--- js/js.js
+++ js/js.js
@@ -17,4 +17,33 @@ function couleur() {
     console.log(div)
 }
 
+
+const boutton = document.querySelector("button")
+
+boutton.addEventListener("click", () => {
+   let value = prompt("Combien de bloc voulez vous ?")
+   if(isNaN(value)){
+    prompt("merci de mettre un chiffre")
+   }
+   else if(value <= 0 || value > 100){
+    prompt("Surprise ! Le chiffre ne doit pas être inéférieur à 0 ou supérieur à 100 !")
+}
+
+    let div = document.querySelectorAll(".container div")
+    div.forEach(div => {
+        div.remove()
+    });
+
+    for (let i = 0; i < value; i++){
+        let div = document.createElement("div");
+        div.style.height = "50px"
+        div.style.width = "50px"
+        div.style.border = "1px solid purple"
+        document.querySelector(".container").appendChild(div)
+    }
+
+    couleur();
+
+})
+
 couleur();
