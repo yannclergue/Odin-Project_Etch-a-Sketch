59a469e1efa395c4b5c181498823df35391370eb
yannclergue <clergue14@gmail.com>
2023-10-17 08:45:46 +0200

    Débuter le js
    Commencement du fichier dans le JS par un "appendChild" pour faire
    apparaitre une div

---
+++ js/js.js
@@ -0,0 +1,7 @@
+let div = document.createElement("div");
+div.style.height = "150px"
+div.style.width = "150px"
+div.style.border = "1px solid black"
+
+
+document.queryselector(".container").appendChild(div)
